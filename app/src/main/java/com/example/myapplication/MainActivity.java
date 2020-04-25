3d6ead3b41fdf2bc329ee8a28990938423a44f2a
180021101078lqy <1961618162@qq.com>
2020-04-25 16:13:38 +0800

    create project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1=(TextView)findViewById(R.id.textView1);
+        tv1.setText("这是我的第一个Android程序：Hello World!");
+    }
+}
